6e06175b8b8565c989aa1289aff48719d5b0d161
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-12-29 18:39:58 +0900

    update

--- src/accessor.rs
+++ src/accessor.rs
@@ -5,10 +5,13 @@ use jelly_mem_access::UioAccessor;
 use std::collections::HashMap;
 use std::error::Error;
 use std::result::Result;
+use std::any::Any;
+
+type Id = u32;
 
 struct Accessor {
-    id: i32,
-    map: HashMap<i32, (Box<dyn MemAccess>, usize)>,
+    id: Id,
+    map: HashMap<Id, (Box<dyn MemAccess>, usize)>,
 }
 
 impl Accessor {
@@ -19,7 +22,7 @@ impl Accessor {
         }
     }
 
-    pub fn open_uio(&mut self, name: &str, unit: usize) -> Result<i32, Box<dyn Error>> {
+    pub fn open_uio(&mut self, name: &str, unit: usize) -> Result<Id, Box<dyn Error>> {
         let accessor = Box::new(UioAccessor::<u8>::new_with_name(name)?);
 
         let id = self.id;
@@ -29,20 +32,34 @@ impl Accessor {
         Ok(id)
     }
 
-//    pub fn subclone(&mut self, name: &str, unit: i32) -> Result<i32, Box<dyn Error>> {
-//    }
-
-    pub fn close(&mut self, id: i32) -> Result<(), Box<dyn Error>> {
-        self.map.remove(&id).ok_or("Invalid id")?;
-        Ok(())
-    }
-
-    fn accessor(&self, id: i32) -> Result<(&Box<dyn MemAccess>, usize), Box<dyn Error>> {
+    fn accessor(&self, id: Id) -> Result<(&Box<dyn MemAccess>, usize), Box<dyn Error>> {
         let (accessor, unit) = self.map.get(&id).ok_or("Invalid id")?;
         Ok((accessor, *unit))
     }
 
-    pub fn write_mem(&self, id: i32, offset: usize, data: usize) -> Result<(), Box<dyn Error>> {
+    /*
+    pub fn subclone(&mut self, id: Id, offset: usize, size: usize, unit: usize) -> Result<Id, Box<dyn Error>> {
+        let (accessor, unit_org) = self.accessor(id)?;
+        let unit = if unit == 0 { unit_org } else { unit };
+        if let Some(&acc) = accessor.as_any().downcast_ref::<UioAccessor<u8>>() {
+            let accessor = acc.subclone::<u8>(offset, size)?;
+            let id = self.id;
+            self.map.insert(id, (accessor, unit));
+            self.id += 1;
+            return Ok(id);
+        }
+
+        Ok(id)
+    }
+    */
+
+    pub fn close(&mut self, id: Id) -> Result<(), Box<dyn Error>> {
+        self.map.remove(&id).ok_or("Invalid id")?;
+        Ok(())
+    }
+
+
+    pub fn write_mem(&self, id: Id, offset: usize, data: usize) -> Result<(), Box<dyn Error>> {
         let (accessor, _) = self.accessor(id)?;
         unsafe {
             accessor.write_mem(offset, data);
@@ -50,7 +67,7 @@ impl Accessor {
         Ok(())
     }
 
-    pub fn write_mem_u8(&self, id: i32, offset: usize, data: u8) -> Result<(), Box<dyn Error>> {
+    pub fn write_mem_u8(&self, id: Id, offset: usize, data: u8) -> Result<(), Box<dyn Error>> {
         let (accessor, _) = self.accessor(id)?;
         unsafe {
             accessor.write_mem_u8(offset, data);
@@ -58,7 +75,7 @@ impl Accessor {
         Ok(())
     }
 
-    pub fn write_reg_u8(&self, id: i32, reg: usize, data: u8) -> Result<(), Box<dyn Error>> {
+    pub fn write_reg_u8(&self, id: Id, reg: usize, data: u8) -> Result<(), Box<dyn Error>> {
         let (accessor, unit) = self.accessor(id)?;
         unsafe {
             accessor.write_mem_u8(reg * unit, data);
@@ -68,7 +85,7 @@ impl Accessor {
 
     pub fn write_mem_u16(
         &mut self,
-        id: i32,
+        id: Id,
         offset: usize,
         data: u16,
     ) -> Result<(), Box<dyn Error>> {
@@ -81,7 +98,7 @@ impl Accessor {
 
     pub fn write_mem_u32(
         &mut self,
-        id: i32,
+        id: Id,
         offset: usize,
         data: u32,
     ) -> Result<(), Box<dyn Error>> {
@@ -94,7 +111,7 @@ impl Accessor {
 
     pub fn write_mem_u64(
         &mut self,
-        id: i32,
+        id: Id,
         offset: usize,
         data: u64,
     ) -> Result<(), Box<dyn Error>> {
