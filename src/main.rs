50433fc893d2980c0edeb19fd141c496dea0cfd8
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-12-31 16:58:20 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -204,19 +204,206 @@ impl FpgaControl for FpgaControlService {
         Ok(Response::new(BoolResponse { result: result.is_ok() }))
     }
 
-
-    async fn write_mem_u64(
+    async fn write_mem_u(
         &self,
-        request: Request<WriteMemU64Request>,
+        request: Request<WriteMemURequest>,
     ) -> Result<Response<BoolResponse>, Status> {
         let req = request.into_inner();
         if self.verbose >= 1 {
-            println!("write_mem_u64:{}", req.id);
+            println!("write_mem:id={} offset={} data={} size={}", req.id, req.offset, req.data, req.size);
         }
         let mut accessor = self.accessor.write().await;
-        let result = accessor.write_mem_u64(req.id as accessor::Id, req.addr as usize, req.data);
+        let result = unsafe { accessor.write_mem_u(req.id as accessor::Id, req.offset as usize, req.data, req.size as usize) };
         Ok(Response::new(BoolResponse { result: result.is_ok() }))
     }
+
+    async fn write_mem_i(
+        &self,
+        request: Request<WriteMemIRequest>,
+    ) -> Result<Response<BoolResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("write_mem:id={} offset={} data={} size={}", req.id, req.offset, req.data, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.write_mem_i(req.id as accessor::Id, req.offset as usize, req.data, req.size as usize) };
+        Ok(Response::new(BoolResponse { result: result.is_ok() }))
+    }
+
+    async fn read_mem_u(
+        &self,
+        request: Request<ReadMemRequest>,
+    ) -> Result<Response<ReadUResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("read_mem_u:id={} offset={} size={}", req.id, req.offset, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.read_mem_u(req.id as accessor::Id, req.offset as usize, req.size as usize) };
+        match result {
+            Ok(data) => Ok(Response::new(ReadUResponse { result: true, data: data })),
+            Err(_) => Ok(Response::new(ReadUResponse { result: false, data: 0 })),
+        }
+    }
+
+    async fn read_mem_i(
+        &self,
+        request: Request<ReadMemRequest>,
+    ) -> Result<Response<ReadIResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("read_mem_u:id={} offset={} size={}", req.id, req.offset, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.read_mem_i(req.id as accessor::Id, req.offset as usize, req.size as usize) };
+        match result {
+            Ok(data) => Ok(Response::new(ReadIResponse { result: true, data: data })),
+            Err(_) => Ok(Response::new(ReadIResponse { result: false, data: 0 })),
+        }
+    }
+
+    async fn write_reg_u(
+        &self,
+        request: Request<WriteRegURequest>,
+    ) -> Result<Response<BoolResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("write_mem:id={} offset={} data={} size={}", req.id, req.reg, req.data, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.write_reg_u(req.id as accessor::Id, req.reg as usize, req.data, req.size as usize) };
+        Ok(Response::new(BoolResponse { result: result.is_ok() }))
+    }
+
+    async fn write_reg_i(
+        &self,
+        request: Request<WriteRegIRequest>,
+    ) -> Result<Response<BoolResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("write_mem:id={} offset={} data={} size={}", req.id, req.reg, req.data, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.write_reg_i(req.id as accessor::Id, req.reg as usize, req.data, req.size as usize) };
+        Ok(Response::new(BoolResponse { result: result.is_ok() }))
+    }
+
+    async fn read_reg_u(
+        &self,
+        request: Request<ReadRegRequest>,
+    ) -> Result<Response<ReadUResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("read_mem_u:id={} offset={} size={}", req.id, req.reg, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.read_reg_u(req.id as accessor::Id, req.reg as usize, req.size as usize) };
+        match result {
+            Ok(data) => Ok(Response::new(ReadUResponse { result: true, data: data })),
+            Err(_) => Ok(Response::new(ReadUResponse { result: false, data: 0 })),
+        }
+    }
+
+    async fn read_reg_i(
+        &self,
+        request: Request<ReadRegRequest>,
+    ) -> Result<Response<ReadIResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("read_reg_i:id={} offset={} size={}", req.id, req.reg, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = unsafe { accessor.read_reg_i(req.id as accessor::Id, req.reg as usize, req.size as usize) };
+        match result {
+            Ok(data) => Ok(Response::new(ReadIResponse { result: true, data: data })),
+            Err(_) => Ok(Response::new(ReadIResponse { result: false, data: 0 })),
+        }
+    }
+
+    /*
+    async fn write_reg_u(
+        &self,
+        request: Request<WriteURequest>,
+    ) -> Result<Response<BoolResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("write_mem:id={} addr={} data={} size={}", req.id, req.addr, req.data, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = match req.size {
+            0 => accessor.write_reg(req.id as accessor::Id, req.addr as usize, req.data as usize),
+            1 => accessor.write_reg_u8(req.id as accessor::Id, req.addr as usize, req.data as u8),
+            2 => accessor.write_reg_u16(req.id as accessor::Id, req.addr as usize, req.data as u16),
+            4 => accessor.write_reg_u32(req.id as accessor::Id, req.addr as usize, req.data as u32),
+            8 => accessor.write_reg_u64(req.id as accessor::Id, req.addr as usize, req.data as u64),
+            _ => return Ok(Response::new(BoolResponse { result: false })),
+        };
+        Ok(Response::new(BoolResponse { result: result.is_ok() }))
+    }
+
+    async fn write_mem_s(
+        &self,
+        request: Request<WriteSRequest>,
+    ) -> Result<Response<BoolResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("write_mem:id={} addr={} data={} size={}", req.id, req.addr, req.data, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = match req.size {
+            0 => accessor.write_mem(req.id as accessor::Id, req.addr as usize, req.data as usize),
+            1 => accessor.write_mem_u8(req.id as accessor::Id, req.addr as usize, req.data as u8),
+            2 => accessor.write_mem_u16(req.id as accessor::Id, req.addr as usize, req.data as u16),
+            4 => accessor.write_mem_u32(req.id as accessor::Id, req.addr as usize, req.data as u32),
+            8 => accessor.write_mem_u64(req.id as accessor::Id, req.addr as usize, req.data as u64),
+            _ => return Ok(Response::new(BoolResponse { result: false })),
+        };
+        Ok(Response::new(BoolResponse { result: result.is_ok() }))
+    }
+
+    async fn write_reg_s(
+        &self,
+        request: Request<WriteSRequest>,
+    ) -> Result<Response<BoolResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("write_mem:id={} addr={} data={} size={}", req.id, req.addr, req.data, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = match req.size {
+            0 => accessor.write_reg(req.id as accessor::Id, req.addr as usize, req.data as usize),
+            1 => accessor.write_reg_u8(req.id as accessor::Id, req.addr as usize, req.data as u8),
+            2 => accessor.write_reg_u16(req.id as accessor::Id, req.addr as usize, req.data as u16),
+            4 => accessor.write_reg_u32(req.id as accessor::Id, req.addr as usize, req.data as u32),
+            8 => accessor.write_reg_u64(req.id as accessor::Id, req.addr as usize, req.data as u64),
+            _ => return Ok(Response::new(BoolResponse { result: false })),
+        };
+        Ok(Response::new(BoolResponse { result: result.is_ok() }))
+    }
+
+    async fn read_mem_u(
+        &self,
+        request: Request<ReadRequest>,
+    ) -> Result<Response<ReadUResponse>, Status> {
+        let req = request.into_inner();
+        if self.verbose >= 1 {
+            println!("read_mem:id={} addr={} size={}", req.id, req.addr, req.size);
+        }
+        let mut accessor = self.accessor.write().await;
+        let result = match req.size {
+            0 => accessor.read_mem    (req.id as accessor::Id, req.addr as usize) as u64,
+            1 => accessor.read_mem_u8 (req.id as accessor::Id, req.addr as usize) as u64,
+            2 => accessor.read_mem_u16(req.id as accessor::Id, req.addr as usize) as u64,
+            4 => accessor.read_mem_u32(req.id as accessor::Id, req.addr as usize) as u64,
+            8 => accessor.read_mem_u64(req.id as accessor::Id, req.addr as usize) as u64,
+            _ => return Ok(Response::new(ReadUResponse { result: false, data : 0 })),
+        };
+        match result {
+            Ok(data) => Ok(Response::new(ReadUResponse { result: true, data: data })),
+            Err(_) => Ok(Response::new(ReadUResponse { result: false, data: 0 })),
+        }
+    }
+    */
 }
 
 #[tokio::main]
