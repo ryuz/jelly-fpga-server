c78df6f94c7b3e30cabc1be97da4584e4909867d
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-10-19 14:45:26 +0900

    fix: update dependencies and correct tonic build invocation in Cargo.toml and build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("./protos/jelly_fpga_control.proto")?;
+    tonic_prost_build::compile_protos("./protos/jelly_fpga_control.proto")?;
     Ok(())
 }
